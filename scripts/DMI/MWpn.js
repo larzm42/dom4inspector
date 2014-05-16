3f1eadcc7526e6cc0ff5675bbfa6393483442087
larzsp <larzsp@gmail.com>
2014-05-16 09:04:22 -0400

    Fix flag parsing.

--- scripts/DMI/MWpn.js
+++ scripts/DMI/MWpn.js
@@ -353,7 +353,7 @@ MWpn.bitfieldValues = function(bitfield, masks_dict) {
 			var flag = "none";
 			var flagIndex = values[value].indexOf("Wpn: #");
 			if (flagIndex != -1) {
-				flag = values[value].substring(flagIndex+6, values[value].length-2)
+				flag = values[value].substring(flagIndex+6, values[value].length-1)
 			}
 			value = values[value].replace(/{(.*?)}/g, "");
 			newValues.push([value, flag]);
